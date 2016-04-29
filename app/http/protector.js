eb9dd5443ef54f9622c1e226799e6bd1f42e0a2d
Jens Bissinger <mail@jens-bissinger.de>
2016-04-29 06:42:07 +0200

    Cleanup protector

--- app/http/protector.js
+++ app/http/protector.js
@@ -3,9 +3,7 @@
 const basicAuth = require('basic-auth')
 const errors = require('./errors')
 
-module.exports = (isAuthorized) => {
-  return (req, res, next) => {
-    const loginAttempt = basicAuth(req)
-    isAuthorized(loginAttempt) ? next() : res.sendError(errors.unauthorized)
-  }
+module.exports = (isAuthorized) => (req, res, next) => {
+  const loginAttempt = basicAuth(req)
+  isAuthorized(loginAttempt) ? next() : res.sendError(errors.unauthorized)
 }

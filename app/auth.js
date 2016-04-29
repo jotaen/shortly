0b3d025fd707b72f414214d6e4b92942abfab008
Jens Bissinger <mail@jens-bissinger.de>
2016-04-29 06:34:33 +0200

    Refactor auth step 2/2

--- app/auth.js
+++ app/auth.js
@@ -1,8 +1,5 @@
 'use strict'
 
-const match = (login, secret) => (typeof login === 'object' && login.name === secret.username && login.pass === secret.password)
-
-module.exports = (username, password) => (login) => match(login, {
-  username: username,
-  password: password
-})
+module.exports = (username, password) => (login) => (
+  typeof login === 'object' && login.name === username && login.pass === password
+)

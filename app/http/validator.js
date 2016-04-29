a0b37ebd7e6f30493f3eb066d9ab3e8a2d8645a9
Jens Bissinger <mail@jens-bissinger.de>
2016-04-29 06:35:24 +0200

    Cleanup validator

--- app/http/validator.js
+++ app/http/validator.js
@@ -2,18 +2,16 @@
 
 const Joi = require('joi')
 
-module.exports = (schema) => {
-  return (req, res, next) => {
-    Joi.validate(req.body, schema, (error) => {
-      if (!error) {
-        next()
-      } else {
-        res.status(422).send({
-          message: 'Error – the request parameters could not be validated.',
-          code: 422,
-          errors: error.details
-        })
-      }
-    })
-  }
+module.exports = (schema) => (req, res, next) => {
+  Joi.validate(req.body, schema, (error) => {
+    if (!error) {
+      next()
+    } else {
+      res.status(422).send({
+        message: 'Error – the request parameters could not be validated.',
+        code: 422,
+        errors: error.details
+      })
+    }
+  })
 }
